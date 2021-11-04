90ee4f44177818270859fad12bc3516cf1d8d7bc
irfanseikh2011 <seikhirfan2011@gmail.com>
2021-11-04 20:52:51 +0530

    fixed bugs and updated the packages

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import React, { Component } from 'react';
-import './App.css';
-import Navigation from './components/Navigation/Navigation';
-import Logo from './components/Logo/Logo';
-import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
-import 'tachyons';
-import Rank from './components/Rank/Rank';
-import Particles from 'react-particles-js';
-import Footer from './components/Footer/Footer';
-import Clarifai from 'clarifai';
-import FaceRecognition from './components/FaceRecognition/FaceRecognition';
-import Signin from './components/SignIn/SignIn';
-import Register from './components/Register/Register';
- 
-
+import React, { Component } from "react";
+import "./App.css";
+import Navigation from "./components/Navigation/Navigation";
+import Logo from "./components/Logo/Logo";
+import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm";
+import "tachyons";
+import Rank from "./components/Rank/Rank";
+import Particles from "react-particles-js";
+import Footer from "./components/Footer/Footer";
+import Clarifai from "clarifai";
+import FaceRecognition from "./components/FaceRecognition/FaceRecognition";
+import Signin from "./components/SignIn/SignIn";
+import Register from "./components/Register/Register";
 
 const app = new Clarifai.App({
-  apiKey: 'cdb8327ad7654350982b3446d1f2c3c0'
- });
-
-
+  apiKey: "cdb8327ad7654350982b3446d1f2c3c0",
+});
 
 const particlesOptions = {
   particles: {
@@ -26,26 +22,26 @@ const particlesOptions = {
       value: 120,
       density: {
         enable: true,
-        value_area: 1000
-      }
+        value_area: 1000,
+      },
     },
     color: {
-      value: "#ffffff"
+      value: "#ffffff",
     },
     shape: {
       type: "circle",
       stroke: {
         width: 20,
-        color: "#ffffff"
+        color: "#ffffff",
       },
       polygon: {
-        nb_sides: 5
+        nb_sides: 5,
       },
       image: {
         src: "img/github.svg",
         width: 100,
-        height: 100
-      }
+        height: 100,
+      },
     },
     opacity: {
       value: 0.4967471076499059,
@@ -54,8 +50,8 @@ const particlesOptions = {
         enable: false,
         speed: 1,
         opacity_min: 0.1,
-        sync: false
-      }
+        sync: false,
+      },
     },
     size: {
       value: 2,
@@ -64,15 +60,15 @@ const particlesOptions = {
         enable: false,
         speed: 40,
         size_min: 0.1,
-        sync: false
-      }
+        sync: false,
+      },
     },
     line_linked: {
       enable: true,
       distance: 150,
       color: "#ffffff",
       opacity: 0.4,
-      width: 1
+      width: 1,
     },
     move: {
       enable: true,
@@ -85,163 +81,181 @@ const particlesOptions = {
       attract: {
         enable: false,
         rotateX: 600,
-        rotateY: 1200
-      }
-    }
+        rotateY: 1200,
+      },
+    },
   },
   interactivity: {
     detect_on: "canvas",
     events: {
       onhover: {
         enable: true,
-        mode: "repulse"
+        mode: "repulse",
       },
       onclick: {
         enable: true,
-        mode: "push"
+        mode: "push",
       },
-      resize: true
+      resize: true,
     },
     modes: {
       grab: {
         distance: 400,
         line_linked: {
-          opacity: 1
-        }
+          opacity: 1,
+        },
       },
       bubble: {
         distance: 500,
         size: 40,
         duration: 2,
         opacity: 8,
-        speed: 3
+        speed: 3,
       },
       repulse: {
         distance: 280,
-        duration: 0.6
+        duration: 0.6,
       },
       push: {
-        particles_nb: 2
+        particles_nb: 2,
       },
       remove: {
-        particles_nb: 1
-      }
-    }
+        particles_nb: 1,
+      },
+    },
   },
-  retina_detect: true
+  retina_detect: true,
 };
-
-
-
-
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      input: '',
-      imageUrl:'',
-      box:{},
-      route:'signin',
+      input: "",
+      imageUrl: "",
+      box: {},
+      route: "signin",
       isSigned: false,
       user: {
-            id: '',
-            name: '',
-            email: '',
-            password: '',
-            entries: 0,
-            joined: new Date()
-      }
-    }
+        id: "",
+        name: "",
+        email: "",
+        password: "",
+        entries: 0,
+        joined: new Date(),
+      },
+    };
   }
 
+  componentDidMount() {
+    fetch("http://localhost:3001")
+      .then((res) => res.json())
+      .then((data) => console.log(data));
+  }
 
   loadUser = (data) => {
-    this.setState({user: {
-      id: data.id,
-      name: data.name,
-      email: data.email,
-      entries: data.entries,
-      joined: data.joined
-    }})
-  }
-
-
+    this.setState({
+      user: {
+        id: data.id,
+        name: data.name,
+        email: data.email,
+        entries: data.entries,
+        joined: data.joined,
+      },
+    });
+  };
 
   calculateFaceLocation = (data) => {
-    const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
-    const image = document.getElementById('inputimage');
+    const clarifaiFace =
+      data.outputs[0].data.regions[0].region_info.bounding_box;
+    const image = document.getElementById("inputimage");
     const width = Number(image.width);
     const height = Number(image.height);
     console.log(width, height);
     return {
-      leftCol : clarifaiFace.left_col * width,
-      topRow : clarifaiFace.top_row * height,
-      rightCol : width - (clarifaiFace.right_col * width),
-      bottomRow : height - (clarifaiFace.bottom_row * height)
-    }
-  }
+      leftCol: clarifaiFace.left_col * width,
+      topRow: clarifaiFace.top_row * height,
+      rightCol: width - clarifaiFace.right_col * width,
+      bottomRow: height - clarifaiFace.bottom_row * height,
+    };
+  };
 
   onInputChange = (event) => {
-    this.setState({input: event.target.value});
-  }
-
+    this.setState({ input: event.target.value });
+  };
 
   displayFaceBox = (box) => {
-    this.setState({box:box});
-  }
-
+    this.setState({ box: box });
+  };
 
   onRouteChange = (route) => {
-    this.setState({route: route});
-    console.log(this.state.users);
-  }
+    this.setState({ route: route });
+  };
 
   onButtonSubmit = () => {
-    this.setState({imageUrl: this.state.input});
+    this.setState({ imageUrl: this.state.input });
     app.models
-    .predict(
-      Clarifai.FACE_DETECT_MODEL,
-      this.state.input)
-    .then(res => 
-      this.displayFaceBox(this.calculateFaceLocation(res)))
-    .catch(err => console.log(err));
-}
+      .predict(Clarifai.FACE_DETECT_MODEL, this.state.input)
+      .then((res) => {
+        if (res) {
+          fetch("http://localhost:3001/image", {
+            method: "put",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({
+              id: this.state.user.id,
+            }),
+          })
+            .then((res) => res.json())
+            .then((count) => {
+              this.setState(Object.assign(this.state.user, { entries: count }));
+            });
+        }
+        this.displayFaceBox(this.calculateFaceLocation(res));
+      })
+      .catch((err) => console.log(err));
+  };
 
-  render(){
-
+  render() {
     const style = {
-      position:'absolute',
-      left:'0',
-      bottom:'0',
-      right:'0',
+      position: "absolute",
+      left: "0",
+      bottom: "0",
+      right: "0",
       height: "60px",
       width: "100%",
-      marginBottom: '2vh',
-     
-  };
+      marginBottom: "2vh",
+    };
+
     return (
       <div className="App">
-       <Particles className='particles'
-       params={particlesOptions}
-        />
-        {this.state.route === 'signin'
-        ? <Signin onRouteChange={this.onRouteChange} />
-        : (this.state.route === 'register') ? <Register onRouteChange={this.onRouteChange} loadUser={this.loadUser} onSignUp={this.onSignUp}/> : <div>
-          <Navigation onRouteChange={this.onRouteChange}/>
-          <Logo />
-          <Rank />
-          <ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit}/>
-          <FaceRecognition imageUrl={this.state.imageUrl} box={this.state.box}/>
-          <Footer style={style} className="footer"/>
-      </div>
-      }
+        <Particles className="particles" params={particlesOptions} />
+        {this.state.route === "signin" ? (
+          <Signin loadUser={this.loadUser} onRouteChange={this.onRouteChange} />
+        ) : this.state.route === "register" ? (
+          <Register
+            onRouteChange={this.onRouteChange}
+            loadUser={this.loadUser}
+            onSignUp={this.onSignUp}
+          />
+        ) : (
+          <div>
+            <Navigation onRouteChange={this.onRouteChange} />
+            <Logo />
+            <Rank name={this.state.user.name} times={this.state.user.entries} />
+            <ImageLinkForm
+              onInputChange={this.onInputChange}
+              onButtonSubmit={this.onButtonSubmit}
+            />
+            <FaceRecognition
+              imageUrl={this.state.imageUrl}
+              box={this.state.box}
+            />
+            <Footer style={style} className="footer" />
+          </div>
+        )}
       </div>
     );
-   }
+  }
 }
-
-
-
 
 export default App;
--- src/components/Rank/Rank.js
+++ src/components/Rank/Rank.js
@@ -1,14 +1,13 @@
-import React from 'react';
+import React from "react";
 
-
-const Rank = () => {
-    return (
-        <div>
-            <div className='white f3'>
-                {'Irfan, you detected #5 faces today.'}
-            </div>
-        </div>
-    );
-}
+const Rank = ({ times, name }) => {
+  return (
+    <div>
+      <div className="white f3">
+        {`${name}, you detected ${times} faces today.`}
+      </div>
+    </div>
+  );
+};
 
 export default Rank;
